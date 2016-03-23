ea65ceba812e1b410400649e6d4fc72c21ce6508
Tim Beswick <contact.tim.here@gmail.com>
2016-03-23 01:33:57 +0000

    Copyright notice

--- src/main/java/com/uksf/tim/utility/Info.java
+++ src/main/java/com/uksf/tim/utility/Info.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) Tim UKSF 2016.
+ * This file is part of UKSF-MM which is released under GPLv3.
+ * Go to https://github.com/tbeswick96/UKSF-MM/blob/master/LICENSE for full license details.
+ */
+
 package com.uksf.tim.utility;
 
 import javax.swing.*;
