195eb940dc395ae6bab9641dcb91df95e78ca237
Tim Beswick <contact.tim.here@gmail.com>
2016-04-12 18:55:27 +0100

    Added connection check

--- src/main/java/com/uksf/mm/core/utility/UpdateWorker.java
+++ src/main/java/com/uksf/mm/core/utility/UpdateWorker.java
@@ -12,6 +12,7 @@ import com.uksf.mm.core.Settings;
 import javax.swing.*;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.ParseException;
 
@@ -19,6 +20,7 @@ import static com.uksf.mm.core.Core.error;
 import static com.uksf.mm.core.Core.nonFatalError;
 import static com.uksf.mm.core.utility.Info.*;
 import static com.uksf.mm.core.utility.LogHandler.Severity.INFO;
+import static com.uksf.mm.core.utility.LogHandler.Severity.WARNING;
 
 /**
  * @author Tim
@@ -32,9 +34,9 @@ public class UpdateWorker extends SwingWorker<Void, Void> {
 	 */
 	@Override
 	protected Void doInBackground() throws Exception {
+		Settings.set("update_time", Settings.weekAhead());
 		LogHandler.logNoTime(HASHSPACE);
 		LogHandler.logSeverity(INFO, "Update check running");
-		Settings.set("update_time", Settings.weekAhead());
 		if(versionCheck()) {
 			LogHandler.logSeverity(INFO, "Update is available. Current version: '" + VERSION + "' Latest version: '" + VERSION_LATEST + "'");
 			if(UPDATE_CHECK || (UPDATE_WEEK && isWeekAhead())) {
@@ -58,15 +60,40 @@ public class UpdateWorker extends SwingWorker<Void, Void> {
 	 */
 	private static boolean versionCheck() {
 		try {
-			URL url = new URL("http://www.uk-sf.com/mm/LATEST.txt"); InputStream stream = url.openStream();
-			StringBuilder buffer = new StringBuilder(""); int character; while(true) {
-				if((character = stream.read()) == - 1) break; buffer.append((char) character);
+			URL url = new URL("http://www.uk-sf.com/mm/LATEST.txt");
+			if(checkConnection(url)) {
+				throw new IOException();
+			}
+			InputStream stream = url.openStream();
+			StringBuilder buffer = new StringBuilder("");
+			int character;
+			while(true) {
+				if((character = stream.read()) == -1) break;
+				buffer.append((char) character);
 			}
 			VERSION_LATEST = buffer.substring(buffer.indexOf("<Version>") + "<Version>".length(), buffer.indexOf("</Version>"));
 			if(VERSION_LATEST.equals(VERSION)) return false;
 		} catch(IOException e) {
-			error(e);
-		} return true;
+			LogHandler.logSeverity(WARNING, "Cannot reach 'www.uk-sf.com', update check not run");
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Checks if connection to URL can be established
+	 * @param url url to check
+	 * @return connection state
+	 * @throws IOException
+	 */
+	private static boolean checkConnection(URL url) throws IOException {
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+		connection.setConnectTimeout(1500);
+		connection.setReadTimeout(1500);
+		connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 (.NET CLR 3.5.30729)");
+		connection.connect();
+		return connection.getResponseCode() == 404;
 	}
 
 	/**
@@ -113,7 +140,10 @@ public class UpdateWorker extends SwingWorker<Void, Void> {
 	 */
 	private static void handleInput(int input) {
 		switch(input) {
-			case JOptionPane.OK_OPTION: runUpdate(); break; case JOptionPane.CANCEL_OPTION: stopShow(); break;
+			case JOptionPane.OK_OPTION:
+				runUpdate(); break;
+			case JOptionPane.CANCEL_OPTION:
+				stopShow(); break;
 		}
 		Core.getInstanceUI().requestFocus();
 	}
